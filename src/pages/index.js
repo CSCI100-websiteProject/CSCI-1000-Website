8885a720fa89af7d19261e8a216eb595605d734f
Simons.Jace10 <Simons.Jace10@svvsd.org>
2022-11-18 13:31:03 -0700

    installed react-scrolling-text package

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Marquee, { Motion, randomIntFromInterval } from "react-marquee-slider";
+import times from "lodash/times";
 
 export default function Home() {
   return (
@@ -9,61 +11,31 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className='title'>Welcome to our group's about me pages!</h1>
-
-        <p className='description'>
-          Get started by editing <code>src/pages/index.js</code> to be your
-          project home page.
-        </p>
-        <p className='description2'>
-          Next, each team member will create their own about me file in the
-          pages directory:{' '}
-        </p>
-        <code>src/page/teamMemberName.js</code>
-
-        <div className='grid'>
-          <a href='/jaceSimons' className='card'>
-            <h3> Jace Simons About Me Page &rarr; </h3>
-            <p> Click here to navigate to Jace Simons' about me page</p>
-          </a>
-          <a href='/renata' className='card'>
-            <h3> Renata Zurita's About Me Page &rarr; </h3>
-            <p> Click here to navigate to Renata Zurita's about me page</p>
-          </a>
-          <a href='/william' className='card'>
-            <h3> William Hardee's About Me Page &rarr; </h3>
-            <p> Click here to navigate to William Hardee's about me page</p>
-          </a>
-            <a href='/mariana' className='card'>
-                <h3>Mariana Vadas-Arendt's About Me Page &rarr;</h3>
-                <p>Click here to navigate to Mariana Vadas-Arendt's about me page</p>
-            </a>
-          <a
-            href='https://github.com/wangalec/website-template'
-            className='card'>
-            <h3>Project Resources&rarr;</h3>
-            <p>
-              Return to the Github for resources on creating your own website!
-            </p>
-          </a>
-          <a
-            href='https://nextjs.org/learn/foundations/about-nextjs'
-            className='card'>
-            <h3>Next.js&rarr;</h3>
-            <p>Click here to learn more about Next.js</p>
-          </a>
-          <a href='https://blog.hubspot.com/website/html' className='card'>
-            <h3>HTML and CSS &rarr;</h3>
-            <p>Click here to learn more about HTML and CSS</p>
-          </a>{' '}
-          <a href='https://tailwindcss.com/' className='card'>
-            <h3>Tailwind css&rarr;</h3>
-            <p>Click here to learn more about tailwind css</p>
-          </a>
-          <a href='https://styled-components.com/' className='card'>
-            <h3>Styled Components&rarr;</h3>
-            <p>Click here to learn more about Styled Components</p>
-          </a>
+        <div style={{ height: "500px" }}>
+  <Marquee velocity={12} minScale={0.7} resetAfterTries={200} scatterRandomly>
+    {times(5, Number).map((id) => (
+      <Motion
+        key={`child-${id}`}
+        initDeg={randomIntFromInterval(0, 360)}
+        direction={Math.random() > 0.5 ? "clockwise" : "counterclockwise"}
+        velocity={10}
+        radius={50}
+      >
+        <div
+          style={{
+            width: "50px",
+            height: "50px",
+            borderRadius: "50%",
+            backgroundColor: "yellow",
+            textAlign: "center",
+            lineHeight: "50px",
+          }}
+        >
+          {id}
+        </div>
+      </Motion>
+    ))}
+  </Marquee>
         </div>
       </main>
 
